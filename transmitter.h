0987ecb477555b35648a209b2528a5f04990934f
RedBoss21 <93236086+RedBoss21@users.noreply.github.com>
2024-01-31 14:52:29 -0600

    Create transmitter.h
    Adding transmitter.h to repo

---
+++ transmitter.h
@@ -0,0 +1,14 @@
+/*
+ * transmitter.h
+ *
+ *  Created on: Jan 31, 2024
+ *      Author: Hampel Matthew
+ */
+
+#ifndef TRANSMITTER_H_
+#define TRANSMITTER_H_
+
+void init_transmitter();
+
+
+#endif
